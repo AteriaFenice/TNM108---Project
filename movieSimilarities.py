0e1f7ba4eb960ab302844837658c267e2ebaded1
Malva <halva.malva@gmail.com>
2022-11-18 10:37:33 +0100

    Added files

---
+++ movieSimilarities.py
@@ -0,0 +1,5 @@
+# MASKININLÄRNING FÖR SOCIALA MEDIER TNM108
+# PROJECT: Use NLP algorithm to decide what movie plots are most similar to eachother
+# AUTHORS: Group 14, Malva Jansson and Maria Brunned
+#____________________________________________________________________________________
+
