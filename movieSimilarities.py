36bdc2be50d321f5eb0b336294ba35852af2fba2
Maria <maria.brunned@gmail.com>
2022-11-21 13:05:32 +0100

    Update movieSimilarities.py

--- movieSimilarities.py
+++ movieSimilarities.py
@@ -3,3 +3,20 @@
 # AUTHORS: Group 14, Malva Jansson and Maria Brunned
 #____________________________________________________________________________________
 
+import numpy as np
+import pandas as pd
+import re 
+import nltk
+
+# Import database
+movies_db = pd.read_csv('imdb (1000 movies) in june 2022.csv')
+
+# Print database
+#print(movies_db.columns)
+
+# Print summary column 
+#print(movies_db['DETAIL ABOUT MOVIE\r\n'])
+
+
+
+
